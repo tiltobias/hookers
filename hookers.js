83a00390c705f744da3a0d892e0081bd9addb458
tiltobias <61829955+tiltobias@users.noreply.github.com>
2021-03-22 10:29:52 +0100

    Add files via upload

--- hookers.js
+++ hookers.js
@@ -1,5 +1,6 @@
 
 
+//-------------------- definerer klasse for hvert produkt som selges i butikken, slik at hvert objekt ser likt ut -----------------
 class product {
     constructor(name,category,price,image){
         this.name = name;
@@ -9,6 +10,7 @@ class product {
         
     };
     
+    //-------------------- Gir produkt-objektet en metode som legger protuktet til i handlelista, eller øker antallet ------------
     addToCart() {
         for (let i = 0; i < shoppingCart.length; i++) {
             if (shoppingCart[i].item === this) {
@@ -24,6 +26,7 @@ class product {
     };
 };
 
+//------------------------ Definerer klasse for hver bestilling som legges til i handlelista, med produkt og antall -------------
 class order {
     constructor(item, quantity){
         this.item = item;
@@ -34,6 +37,7 @@ class order {
 
 const shoppingCart = [];
 
+//------------------------ 
 function purchaseCart(customCart) {
     let cart = customCart || shoppingCart;
     console.log(cart);
@@ -52,10 +56,13 @@ function purchaseCart(customCart) {
 };
 
 
+//--------------------- Lagrer alle butikkens produkter i ett objekt, slik at dette er letter å finne spesifike produkter enn i en array, og mer oversiktlig enn bare som individuelle variabler. ---
+const stock = {};
+stock.hook1 = new product("hook1", "hooks", 50, "hook1.png");
+stock.hook2 = new product("hook2", "hooks", 60, "hook2.png");
+stock.rod = new product("Fishingrod", "rods", 200, "rod.png");
 
 
-let hook1 = new product("hook1", "hooks", 50, "hook1.png");
-let hook2 = new product("hook2", "hooks", 60, "hook2.png");
-let rod = new product("Fishingrod", "rods", 200, "rod.png");
+
 
 
